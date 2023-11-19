9d0bb559b85cbb2f1dc8eee0af30eea1a8911a43
CRaez-Dev <craez.dev@gmail.com>
2023-11-19 18:45:44 -0500

    Adding class 2.14 Lambdas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,7 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Mic
 import androidx.compose.runtime.*
@@ -14,49 +11,80 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlin.concurrent.thread
 
 class AppState {
-    val notes = mutableStateOf(getNotes())
+
+    val state = mutableStateOf(UiState())
+
+    fun loadNotes() {
+        thread {
+
+            state.value = UiState(loading = true)
+            getNotes { it -> state.value = UiState(notes = it, loading = false) }
+        }
+    }
+
+    data class UiState(
+        val notes: List<Note> = emptyList(),
+        val loading: Boolean = false
+    )
 }
 
 @Composable
 @Preview
 fun App(appState: AppState) {
 
+    LaunchedEffect(true) {
+        appState.loadNotes()
+    }
+    val notes = appState.state.value
+
     MaterialTheme {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(
+            contentAlignment = Alignment.Center,
         ) {
-            items(appState.notes.value) {
-                Card(
+            if(notes.loading) CircularProgressIndicator()
+            NotesList(notes.notes)
+        }
+
+    }
+}
+
+@Composable
+private fun NotesList(notes: List<Note>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        items(notes) {
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(0.8f)
+            ) {
+                Column(
                     modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth(0.8f)
+                        .padding(16.dp)
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(16.dp)
-                    ) {
 
-                        Row {
-                            Text(
-                                style = MaterialTheme.typography.h6,
-                                text = it.title,
-                                modifier = Modifier.weight(1f)
+                    Row {
+                        Text(
+                            style = MaterialTheme.typography.h6,
+                            text = it.title,
+                            modifier = Modifier.weight(1f)
+                        )
+                        if (it.type == Note.Type.AUDIO) {
+                            Icon(
+                                imageVector = Icons.Default.Mic,
+                                contentDescription = null
                             )
-                            if (it.type == Note.Type.AUDIO) {
-                                Icon(
-                                    imageVector = Icons.Default.Mic,
-                                    contentDescription = null
-                                )
-                            }
-
                         }
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(it.description)
+
                     }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(it.description)
                 }
             }
         }
