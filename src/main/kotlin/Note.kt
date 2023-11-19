b3cd9107900e676b59d01441bdf81bc842d24115
CRaez-Dev <craez.dev@gmail.com>
2023-11-18 21:36:02 -0500

    Adding class 2.9 Enums

--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -1,1 +1,23 @@
-data class Note( val title:String,val description :String)
+/*enum class Type(val id:Int, val description: String) {
+    TEXT(1,"text") ,
+    AUDIO(0,"audio")
+}*/
+data class Note( val title:String,val description :String,val type:Type){
+    enum class Type(val id:Int, val description: String) {
+        TEXT(1,"text") ,
+        AUDIO(0,"audio")
+    }
+}
+
+val list: MutableList<Note> = mutableListOf(
+    Note("Title 1", "Description 1",Note.Type.TEXT),
+    Note("Title 2", "Description 2",Note.Type.AUDIO),
+    Note("Title 3", "Description 3",Note.Type.AUDIO),
+    Note("Title 4", "Description 4",Note.Type.AUDIO),
+    Note("Title 5", "Description 5",Note.Type.TEXT),
+    Note("Title 6", "Description 6",Note.Type.TEXT),
+    Note("Title 7", "Description 7",Note.Type.AUDIO),
+    Note("Title 8", "Description 8",Note.Type.AUDIO),
+    Note("Title 9", "Description 9",Note.Type.TEXT),
+    Note("Title 10", "Description 10",Note.Type.TEXT)
+)
