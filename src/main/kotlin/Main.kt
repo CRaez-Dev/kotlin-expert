8b902447d35c17acca863cadbb4eb32af934b690
CRaez-Dev <craez.dev@gmail.com>
2023-11-19 17:37:09 -0500

    Adding class 2.13 Adding UI to notes application

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,58 +1,73 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-
-
-fun buildMessage(msg: String): String = "Sup $msg !"
-
-class stateView {
-    var message = mutableStateOf("")
-
-    fun isButtonEnable() =  message.value.isNotEmpty()
-    /*
-    val buttonEnabled: Boolean
-        get() = message.value.isNotEmpty()
-    * */
+class AppState {
+    val notes = mutableStateOf(getNotes())
 }
 
 @Composable
 @Preview
-fun App(state: stateView) {/*
-    -   Cuando necesitas un estado que la interfaz valla a utilizar para actualizarce
-        usa el remember mutableStateOf para que el valor sobreviva a las recomposiciones de compose
-
-    -   remember es como un memo en react
-    var text by remember { mutableStateOf("Hello, Raez World!") }
-    */
-
-    // val text = remember { mutableStateOf("") }
-    // val message = "Sup! ${text.value}"
-
+fun App(appState: AppState) {
 
     MaterialTheme {
-        Column {
-            TextField(value = state.message.value, onValueChange = { newText -> state.message.value = newText  })
-            Text(text = buildMessage(state.message.value))
-            Button(onClick = { state.message.value ="" }, enabled = state.isButtonEnable()) {
-                Text("Clean")
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(appState.notes.value) {
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(0.8f)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                    ) {
+
+                        Row {
+                            Text(
+                                style = MaterialTheme.typography.h6,
+                                text = it.title,
+                                modifier = Modifier.weight(1f)
+                            )
+                            if (it.type == Note.Type.AUDIO) {
+                                Icon(
+                                    imageVector = Icons.Default.Mic,
+                                    contentDescription = null
+                                )
+                            }
+
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(it.description)
+                    }
+                }
             }
         }
     }
 }
 
-
 fun main() {
-    val state = stateView()
+    val appState = AppState()
     application {
         Window(onCloseRequest = ::exitApplication, title = "My Application") {
-            App(state)
+            App(appState)
         }
     }
 }
