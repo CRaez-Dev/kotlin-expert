5f8897732e6226eb619996e1d2c01b95a2f51b26
CRaez-Dev <craez.dev@gmail.com>
2023-11-18 20:28:39 -0500

    Adding class 2.6 Classes properties and backing fields

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,57 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+
+fun buildMessage(msg: String): String = "Sup $msg !"
+
+class stateView {
+    var message = mutableStateOf("")
+
+    fun isButtonEnable() =  message.value.isNotEmpty()
+    /*
+    val buttonEnabled: Boolean
+        get() = message.value.isNotEmpty()
+    * */
+}
+
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun App(state: stateView) {/*
+    -   Cuando necesitas un estado que la interfaz valla a utilizar para actualizarce
+        usa el remember mutableStateOf para que el valor sobreviva a las recomposiciones de compose
+
+    -   remember es como un memo en react
+    var text by remember { mutableStateOf("Hello, Raez World!") }
+    */
+
+    // val text = remember { mutableStateOf("") }
+    // val message = "Sup! ${text.value}"
+
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            TextField(value = state.message.value, onValueChange = { newText -> state.message.value = newText  })
+            Text(text = buildMessage(state.message.value))
+            Button(onClick = { state.message.value ="" }, enabled = state.isButtonEnable()) {
+                Text("Clean")
+            }
         }
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+
+fun main() {
+    val state = stateView()
+    application {
+        Window(onCloseRequest = ::exitApplication, title = "My Application") {
+            App(state)
+        }
     }
 }
