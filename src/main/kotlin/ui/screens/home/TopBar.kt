361e88ab5056bdd1957ab8a5205aa3f3ad61c33c
CRaez-Dev <craez.dev@gmail.com>
2024-01-07 15:25:59 -0500

    Adding class 4.5 - Reverse last commit, It was just for test infix functions

--- src/main/kotlin/ui/screens/home/TopBar.kt
+++ src/main/kotlin/ui/screens/home/TopBar.kt
@@ -21,17 +21,6 @@ fun TopBar(onFilterClick:(Filter)->Unit) {
 @Composable
 private fun FilterAction(onFilterClick:(Filter)->Unit) {
     var expanded by remember { mutableStateOf(false) }
-
-    @Composable
-    infix fun Filter.ToMenuItem(text:String){
-        DropdownMenuItem(onClick = {
-            onFilterClick(this)
-            expanded = false
-        }) {
-            Text(text = text)
-        }
-    }
-
     IconButton(onClick = { expanded = true }) {
         Icon(
             imageVector = Icons.Default.FilterList,
@@ -40,10 +29,6 @@ private fun FilterAction(onFilterClick:(Filter)->Unit) {
 
         DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
 
-            Filter.All ToMenuItem "All"
-            Filter.ByType(Note.Type.TEXT) ToMenuItem "Text"
-            Filter.ByType(Note.Type.AUDIO) ToMenuItem "Audio"
-            /*
             listOf(
                 Filter.All to "All",
                 Filter.ByType(Note.Type.TEXT) to "Text",
@@ -56,7 +41,6 @@ private fun FilterAction(onFilterClick:(Filter)->Unit) {
                     Text(text = second)
                 }
             }
-             */
         }
     }
 }
