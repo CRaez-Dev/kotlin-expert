31bdf0bb63edba6154acee50a3474f81075b7f51
CRaez-Dev <craez.dev@gmail.com>
2024-01-07 15:24:05 -0500

    Adding class 4.3 - Local functions

--- src/main/kotlin/ui/screens/home/TopBar.kt
+++ src/main/kotlin/ui/screens/home/TopBar.kt
@@ -21,6 +21,17 @@ fun TopBar(onFilterClick:(Filter)->Unit) {
 @Composable
 private fun FilterAction(onFilterClick:(Filter)->Unit) {
     var expanded by remember { mutableStateOf(false) }
+
+    @Composable
+    infix fun Filter.ToMenuItem(text:String){
+        DropdownMenuItem(onClick = {
+            onFilterClick(this)
+            expanded = false
+        }) {
+            Text(text = text)
+        }
+    }
+
     IconButton(onClick = { expanded = true }) {
         Icon(
             imageVector = Icons.Default.FilterList,
@@ -29,6 +40,10 @@ private fun FilterAction(onFilterClick:(Filter)->Unit) {
 
         DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
 
+            Filter.All ToMenuItem "All"
+            Filter.ByType(Note.Type.TEXT) ToMenuItem "Text"
+            Filter.ByType(Note.Type.AUDIO) ToMenuItem "Audio"
+            /*
             listOf(
                 Filter.All to "All",
                 Filter.ByType(Note.Type.TEXT) to "Text",
@@ -41,6 +56,7 @@ private fun FilterAction(onFilterClick:(Filter)->Unit) {
                     Text(text = second)
                 }
             }
+             */
         }
     }
 }
